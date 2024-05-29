0be3aa17d05da988fc2dc514873f88e461b7b961
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2024-05-29 22:47:58 +0300

    err

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.use("/api/v1/categories", require("./category.route")); // DONE
 router.use("/api/v1/service", require("./service.route")); // DONE
 router.use("/api/v1/review", require("./review.route")); // DONE
 router.use("/api/v1/wishlist", require("./wishlist.route")); // DONE
-router.use("/api/v1/address", require("./address.route")); // DONE
-router.use("/api/v1/coupon", require("./coupon.route")); // DONE
+// router.use("/api/v1/address", require("./address.route")); // DONE
+// router.use("/api/v1/coupon", require("./coupon.route")); // DONE
 router.use("/api/v1/booking", require("./booking.route")); // DONE
 router.use("/api/v1/transaction", require("./transaction.route")); // DONE
 router.use("/api/v1/chat", require("./chat.route")); // DONE
